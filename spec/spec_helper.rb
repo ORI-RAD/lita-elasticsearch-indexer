124e03dbfe587e8575037514af52b0557c91612a
Darrin Mann <darrin.mann@duke.edu>
2016-12-14 18:03:04 -0500

    The spec tests need redis configured.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,8 @@ require "lita/rspec"
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
 # was generated with Lita 4, the compatibility mode should be left disabled.
 Lita.version_3_compatibility_mode = false
+
+Lita.configure do |config|
+  config.redis[:host] = ENV['LITA_REDIS_HOST']
+  # config.redis.port = 1234
+end
