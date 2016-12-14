06302f5a75cc576b1fe77b22d1e42ee81b3d873f
Darrin Mann <darrin.mann@duke.edu>
2016-12-14 18:06:07 -0500

    Initial tests for what we have already written.

--- spec/lita/handlers/elasticsearch_indexer_spec.rb
+++ spec/lita/handlers/elasticsearch_indexer_spec.rb
@@ -1,4 +1,21 @@
 require "spec_helper"
+require "faker"
 
 describe Lita::Handlers::ElasticsearchIndexer, lita_handler: true do
+  let(:config) { registry.config.handlers.elasticsearch_indexer }
+  let(:message) { Faker::Hacker.say_something_smart }
+
+  it { is_expected.to route(message).to(:index_conversation) }
+  it { is_expected.not_to route('').to(:index_conversation) }
+
+  describe 'config' do
+    it { expect(config).to respond_to(:elasticsearch_url) }
+  end
+
+  describe '#index_conversation' do
+    before do
+      config.elasticsearch_url = ENV['LITA_ELASTICSEARCH_URL']
+    end
+    it { is_expected.to respond_to(:index_conversation).with(1).argument }
+  end
 end
