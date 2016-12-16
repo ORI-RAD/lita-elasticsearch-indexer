3d1b12b5ec918cd937bd2d23d2052e7677483bdb
Darrin Mann <darrin.mann@duke.edu>
2016-12-16 13:59:47 -0500

    Reorganizing specs around message state as a context groups

--- spec/lita/handlers/elasticsearch_indexer_spec.rb
+++ spec/lita/handlers/elasticsearch_indexer_spec.rb
@@ -2,11 +2,6 @@ require "spec_helper"
 require "faker"
 
 describe Lita::Handlers::ElasticsearchIndexer, lita_handler: true do
-  let(:message) { Faker::Hacker.say_something_smart }
-
-  it { is_expected.to route(message).to(:index_conversation) }
-  it { is_expected.not_to route('').to(:index_conversation) }
-
   describe 'config' do
     let(:config) { Hash[described_class.configuration_builder.children.collect {|x| [x.name, x]}] }
     # Elasticsearch::Transport Client setting hosts documentation:
@@ -23,10 +18,29 @@ describe Lita::Handlers::ElasticsearchIndexer, lita_handler: true do
   end
 
   describe '#index_conversation' do
-    let(:registry_config) { registry.config.handlers.elasticsearch_indexer }
-    before do
-      registry_config.elasticsearch_url = ENV['LITA_ELASTICSEARCH_URL']
-    end
     it { is_expected.to respond_to(:index_conversation).with(1).argument }
+
+    context 'with a non-empty message' do
+      let(:message) { Faker::Hacker.say_something_smart }
+
+      it { is_expected.to route(message).to(:index_conversation) }
+
+      context 'send_message' do
+        let(:registry_config) { registry.config.handlers.elasticsearch_indexer }
+        let(:index_name) { 'lita-handler-test' }
+        before do
+          registry_config.elasticsearch_url = ENV['LITA_ELASTICSEARCH_URL']
+          registry_config.elasticsearch_index_name = index_name
+
+          expect(registry_config.elasticsearch_url).not_to be_nil
+        end
+
+        it { expect{ send_message(message) }.not_to raise_error }
+      end
+    end
+
+    context 'with an empty message' do
+      it { is_expected.not_to route('').to(:index_conversation) }
+    end
   end
 end
